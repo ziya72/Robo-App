4b40fdf5d9f252b1ae1923c9d89a3c46b74679de
fatimaarif19 <fatima.arif13071989@gmail.com>
2025-07-02 22:08:41 +0530

    edit in main.dart

--- robo/lib/main.dart
+++ robo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:robo/navigation.dart';
+import 'package:robo/components/component_form.dart';
+import 'package:robo/components/membership_form.dart';
+import 'package:robo/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,31 +10,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'RoboClub',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.black,
+        useMaterial3: true,
       ),
-      home: AppBottomNavigationBar(),
+      home: HomePage(),
+      routes: {
+        '/components/membership_form': (context) => RegistrationForm(),
+        '/components/component_form': (context) => ComponentForm(),
+      },
     );
   }
 }
